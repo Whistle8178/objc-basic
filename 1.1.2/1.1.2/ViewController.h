fe0a9c3bf1f6b14cc10fa444ccd2e17298b69429
竹村鷹基 <car2922003@gmail.com>
2018-02-05 17:03:58 +0900

    [add]1.1.2が完了しました

---
+++ 1.1.2/1.1.2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1.1.2
+//
+//  Created by 竹村鷹基 on 2018/02/05.
+//  Copyright © 2018年 竹村鷹基. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 1.1.2/1.1.2/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  1.1.2
+//
+//  Created by 竹村鷹基 on 2018/02/04.
+//  Copyright © 2018年 竹村鷹基. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+int main(int argc, const char * argv[])
+{
+    NSArray *nums = @[@16, @15, @25, @54];
+    NSLog(@"nums= %@,nums");
+    
+    NSDictionary *cours = @{@"A":@"水泳",@"B":@"バイク",@"C":@"ヨガ",@"D":@"サッカー"};
+    NSLog(@"Aコース%@",cours[@"A"]);
+    NSLog(@"Cコース%@",cours[@"C"]);
+    NSLog(@"コース%@",cours[@"D"]);
+    return 0;
+}
