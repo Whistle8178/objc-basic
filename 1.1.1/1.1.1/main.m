d339983cac1f090d2828177266462a217e6d425a
竹村鷹基 <car2922003@gmail.com>
2018-02-05 16:42:41 +0900

    [add] 1.1.1.の課題が完了しました。

---
+++ 1.1.1/1.1.1/main.m
@@ -0,0 +1,41 @@
+//
+//  main.m
+//  1.1.1
+//
+//  Created by 竹村鷹基 on 2018/02/04.
+//  Copyright © 2018年 竹村鷹基. All rights reserved.
+//
+
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    BOOL isDone;
+    isDone = YES;
+    if(isDone){
+        NSLog(@"Ok");
+    }else{
+        NSLog(@"None");
+    }
+    
+    NSString *msg;
+    
+    NSDate *now;
+    
+    msg = @"thankyou";
+    now = [NSDate date];
+    
+    NSLog(@"%@ %@",msg,now);
+    
+    NSInteger price;
+    price = 100;
+    NSLog (@"お会計は%ld円になります。",(long)price);
+    
+    NSArray *nums = @[@16, @15, @5, @25, @78];
+    
+    NSLog(@"nums= %@" , nums);
+    
+    return 0;
+}
