a232eece975a1d8b0dcd8c70fb4d9f2e71fd7697
竹村鷹基 <car2922003@gmail.com>
2018-02-07 00:36:33 +0900

    [add]2.1.1.の課題が完了しました。

---
+++ 2.1.1/2.1.1/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  2.1.1
+//
+//  Created by 竹村鷹基 on 2018/02/06.
+//  Copyright © 2018年 竹村鷹基. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+
+
+@end
+
---
+++ 2.1.1/2.1.1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  2.1.1
+//
+//  Created by 竹村鷹基 on 2018/02/06.
+//  Copyright © 2018年 竹村鷹基. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
