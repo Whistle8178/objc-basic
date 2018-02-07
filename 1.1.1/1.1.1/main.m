8f3a5da674e1231f23cc16786c2ba1452fb63ef5
竹村鷹基 <car2922003@gmail.com>
2018-02-07 19:21:49 +0900

    [Fix]インスタンス化の時点で値を入れるように修正しました。

--- 1.1.1/1.1.1/main.m
+++ 1.1.1/1.1.1/main.m
@@ -12,25 +12,19 @@
 
 int main(int argc, const char * argv[])
 {
-    BOOL isDone;
-    isDone = YES;
+    BOOL isDone = YES;
     if(isDone){
         NSLog(@"Ok");
     }else{
         NSLog(@"None");
     }
     
-    NSString *msg;
+    NSString *msg = @"thankyou";
     
-    NSDate *now;
-    
-    msg = @"thankyou";
-    now = [NSDate date];
-    
+    NSDate *now = [NSDate date];
     NSLog(@"%@ %@",msg,now);
     
-    NSInteger price;
-    price = 100;
+    NSInteger price = 100;
     NSLog (@"お会計は%ld円になります。",(long)price);
     
     NSArray *nums = @[@16, @15, @5, @25, @78];
