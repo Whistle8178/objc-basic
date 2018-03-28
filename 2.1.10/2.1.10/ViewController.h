c7daf0a049c9762212d112b42ef30ecf87beedb0
竹村鷹基 <car2922003@gmail.com>
2018-03-29 03:10:51 +0800

    [2.1.10]の課題が完了しました。

--- 2.1.10/2.1.10/ViewController.h
+++ 2.1.10/2.1.10/ViewController.h
@@ -1,15 +1,15 @@
 //
 //  ViewController.h
-//  1.1.5
+//  2.1.10
 //
-//  Created by 竹村鷹基 on 2018/03/01.
+//  Created by 竹村鷹基 on 2018/03/11.
 //  Copyright © 2018年 竹村鷹基. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
